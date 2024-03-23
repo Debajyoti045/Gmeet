097f32db962e65fef12bd12308f32c382b4a6646
Debajyoti045 <debajyotidas497@gmail.com>
2024-03-23 16:34:36 +0530

    second commit

--- src/Components/Message.jsx
+++ src/Components/Message.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Message({ messages }) {
   return (
-     <div style={{display:"inline"}}>
+     <div style={{color:"white",fontSize:"30px",display:"block"}}>
           {messages}
      </div>
   );
--- src/Components/User.jsx
+++ src/Components/User.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import shareImage from "./share.jpg";
+import personImage from "./person.jpg";
 export default function User({ name }) {
   return (
     <div style={{ background: "white", height: "40vh", width: "40vh", marginTop: "8px",marginLeft:"70px",marginRight:"20px",marginBottom:"5px"}}>
       <p style={{textAlign:"center"}}>{name}</p>
-      <img style={{height:"150px",marginTop:"20px",marginLeft:"60px"}} src={shareImage} alt="pic" />
+      <img style={{height:"150px",marginTop:"20px",marginLeft:"60px"}} src="https://thumbs.dreamstime.com/b/silhouette-person-icon-black-circular-frame-silhouette-person-icon-black-circular-frame-vector-graphic-218821794.jpg" alt="pic" />
     </div>
   );
 }
